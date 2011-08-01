c813da49fd13b9f8397844d5ed6ebb42b28cad59
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-07-31 22:08:13 -0500

    Simplified the ConsoleChat plugin by stripping out groups
    The group stuff was useful when I first created the library, but now it has
    become an encumbrance. Removing it.

--- lib/Bot/Backbone/Service/ConsoleChat.pm
+++ lib/Bot/Backbone/Service/ConsoleChat.pm
@@ -6,7 +6,6 @@ with qw(
     Bot::Backbone::Role::Service
     Bot::Backbone::Role::Dispatch
     Bot::Backbone::Role::Chat
-    Bot::Backbone::Role::GroupJoiner
 );
 
 use Bot::Backbone::Message;
@@ -17,9 +16,7 @@ use POE qw( Wheel::ReadLine );
 =head1 SYNOPSIS
 
   service console => (
-      service      => 'ConsoleChat',
-      bot_username => 'coolbot',
-      bot_nickname => 'Cool Bot',
+      service  => 'ConsoleChat',
   );
 
 =head1 DESCRIPTION
@@ -54,54 +51,48 @@ sub _build_term {
     );
 };
 
-=head2 bot_username
+=head2 prompt
 
-The username to give the bot.
-
-=cut
-
-has bot_username => (
-    is          => 'ro',
-    isa         => 'Str',
-    required    => 1,
-);
-
-=head2 bot_nickname
-
-The nickname to give the bot.
+This is the prompt displayed to the user. It is "> " by default. You may set
+this during initialization or the bot may modify it as desired.
 
 =cut
 
-has bot_nickname => (
-    is          => 'ro',
-    isa         => 'Str',
-    required    => 1,
-);
-
-=head2 current_group
-
-The console chat provides groups for chat. This is mostly to allow it to work
-seamlessly with the multicast chat.
-
-When chat is in a group, any message the bot sends to that group will be
-displayed. Also, interactive commands sent will be presented as if they happened
-in that group.
-
-The special group "(all)" means that the console is in no group, but shows all
-gropu chats that the bot may send on the console. Anything put on the command
-line will behave as if they are direct messages to the bot.
-
-The special group "(none)" means that the console is in no group. This mutes all
-group chats that the bot may send and all entries on the command line will
-behave as if they are direct messages to the bot.
-
-=cut
-
-has current_group => (
+has prompt => (
     is          => 'rw',
     isa         => 'Str',
     required    => 1,
-    default     => '(all)',
+    default     => '> ',
+);
+
+=head2 username
+
+This is the name of the user at the console. This is set to C<$ENV{USER}> by
+default.
+
+=cut
+
+has username => (
+    is          => 'ro',
+    isa         => 'Str',
+    required    => 1,
+    default     => sub { $ENV{USER} },
+);
+
+=head2 nickname
+
+This is the nickname to give the user at the console. As of now, this is also se to
+C<$ENV{USER}> be default.
+
+It may be set to gecos or something by default in the future.`
+
+=cut
+
+has nickname => (
+    is          => 'ro',
+    isa         => 'Str',
+    required    => 1,
+    default     => sub { $ENV{USER} },
 );
 
 =head1 EVENT HANDLERS
@@ -125,162 +116,65 @@ sub _start {
 Whenever the user hits enter, this dispatches, notifies the chat consumers, and
 puts up the next prompt.
 
-Anything not matching a built-in command will be passed to the bot as if it were
-a direct message or posted to the L</current_group>.
-
+Anything not matching a built-in command will be passed to the bot as a direct
+message.  
 The built-in commands include:
 
 =head3 /quit
 
 This causes the bot to shutdown and exit.
 
-=head3 /join
-
-  /join group
-
-Given the name of a group, the console changes the L</current_group> that that
-group name.
-
-=head3 /leave
-
-Causes the console chat to enter the "(all)" group. 
-
-=head3 /mute
-
-Causes the console chat to enter the "(none)" group.
-
-See L</current_group>.
-
-=head3 /dm
-
-  /dm ...
-
-This is implied when the L</current_group> is in "(all)" or "(none)". This
-causes a message typed at the prompt to be treated as a direct message, even if
-L</current_group> is set to a specific group.
-
 =cut
 
 sub got_console_input {
-    my ($self, $input, $exception) = @_[OBJECT, ARG0, ARG1];
+    my ($self, $p, $input, $exception) = @_[OBJECT, KERNEL, ARG0, ARG1];
 
     return unless defined $input;
 
     my $term = $self->term;
     if ($input eq '/quit') {
-        $term->addhistory($input);
         $self->bot->shutdown;
         return;
     }
-    elsif ($input =~ m{^/join\s+(\w+)}) {
-        $term->addhistory($input);
-        $self->current_group($1);
-        $input = '';
-    }
-    elsif ($input =~ m{^/leave$}) {
-        $term->addhistory($input);
-        $self->current_group('(all)');
-        $input = '';
-    }
-    elsif ($input =~ m{^/mute$}) {
-        $term->addhistory($input);
-        $self->current_group('(none)');
-        $input = '';
-    }
-    elsif ($input =~ m{^/gm\s+(\S+)\s+(.+)}) {
-        my $group = $1;
-        my $text  = $2;
-
-        $term->addhistory($input);
-
-        my $message = Bot::Backbone::Message->new({
-            chat  => $self,
-            from  => Bot::Backbone::Identity->new(
-                username => '(console)',
-                nickname => '(console)',
-            ),
-            to    => Bot::Backbone::Identity->new(
-                username => $self->bot_username,
-                nickname => $self->bot_nickname,
-            ),
-            group => $1,
-            text  => $2,
-        });
-
-        $self->resend_message($message);
-
-        if ($self->has_dispatcher) {
-            $self->dispatch_message($message);
-        }
-    }
-    elsif ($input =~ s{^/dm\s+(.+)}{$1}) {
-        $term->addhistory($input);
-
-        my $message = Bot::Backbone::Message->new({
-            chat  => $self,
-            from  => Bot::Backbone::Identity->new(
-                username => '(console)',
-                nickname => '(console)',
-            ),
-            to    => Bot::Backbone::Identity->new(
-                username => $self->bot_username,
-                nickname => $self->bot_nickname,
-            ),
-            group => undef,
-            text  => $input,
-        });
-
-        $self->resend_message($message);
-
-        if ($self->has_dispatcher) {
-            $self->dispatch_message($message);
-        }
-    }
     else {
-        my $group = $self->current_group;
-           $group = undef if $group eq '(none)'
-                          or $group eq '(all)';
-
-        my $message = Bot::Backbone::Message->new({
-            chat  => $self,
-            from  => Bot::Backbone::Identity->new(
-                username => '(console)',
-                nickname => '(console)',
-            ),
-            to    => Bot::Backbone::Identity->new(
-                username => $self->bot_username,
-                nickname => $self->bot_nickname,
-            ),
-            group => $group,
-            text  => $input,
-        });
-
-        $self->resend_message($message);
-
-        if ($self->has_dispatcher) {
-            $self->dispatch_message($message);
-        }
+        $p->yield('cli_message', $input);
     }
 
-    if ($input ne '') {
-        $term->addhistory($input);
-        $term->put($input);
-    }
+    $term->addhistory($input) if $input =~ /\S/;
     $term->get($self->prompt);
 }
 
-=head1 METHODS
+=head2 cli_mesage
 
-=head2 prompt
-
-This is used to draw the prompt.
+Handles most messages typed on the command line.
 
 =cut
 
-sub prompt {
-    my $self = shift;
-    return $self->current_group . ' > ';
+sub cli_message {
+    my ($self, $text) = @_[OBJECT,ARG0];
+
+    my $message = Bot::Backbone::Message->new({
+        chat  => $self,
+        from  => Bot::Backbone::Identity->new(
+            username => $self->username,
+            nickname => $self->nickname,
+        ),
+        to    => Bot::Backbone::Identity->new(
+            username => '(console)',
+            nickname => '(console)',
+        ),
+        group => undef,
+        text  => $text,
+    });
+
+    $self->resend_message($message);
+
+    if ($self->has_dispatcher) {
+        $self->dispatch_message($message);
+    }
 }
+
+=head1 METHODS
 
 =head2 initialize
 
@@ -293,19 +187,14 @@ sub initialize {
 
     POE::Session->create(
         object_states => [
-            $self => [ qw( _start got_console_input ) ],
+            $self => [ qw( 
+                _start 
+                got_console_input 
+                cli_message
+            ) ],
         ],
     );
 }
-
-=head2 join_group
-
-Does nothing. The bot can communicate with a group whether joined or not, we
-don't especially care.
-
-=cut
-
-sub join_group { }
 
 =head2 send_reply
 
@@ -315,7 +204,7 @@ Whenever the bot replies to a message, the reply will be posted to console.
 
 sub send_reply {
     my ($self, $message, $text) = @_;
-    $self->term->put($self->bot_username . ': ' . $text);
+    $self->term->put($text);
 }
 
 =head2 send_message
@@ -329,21 +218,8 @@ other messages will be muted.
 sub send_message {
     my ($self, %params) = @_;
 
-    my $to    = $params{to};
-    my $group = $params{group};
-    my $text  = $params{text};
-
-    if (defined $group) {
-        return unless $group eq $self->current_group;
-    }
-    elsif (defined $to) {
-        return unless $to->username eq '(console)';
-    }
-    else {
-        return;
-    }
-
-    $self->term->put($self->bot_username . ': ' . $text);
+    my $text = $params{text};
+    $self->term->put($text);
 }
 
 =head2 shutdown
