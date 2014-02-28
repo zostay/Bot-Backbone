3b14fcb948ae4afd19a6fcc43b9d09184b02283d
Sterling Hanenkamp <hanenkamp@cpan.org>
2014-02-28 10:19:44 -0600

    Fix the nick removal of is_to_me by making the text a reference

--- lib/Bot/Backbone/Service/JabberChat.pm
+++ lib/Bot/Backbone/Service/JabberChat.pm
@@ -436,9 +436,11 @@ sub got_direct_message {
 
 =head2 is_to_me
 
-  my $bool = $self->is_to_me($user, $text);
+  my $bool = $self->is_to_me($user, \$text);
 
-Given the user that identifies the bot in a group chat and text that was just sent to the chat, this detects if the message was directed at the bot. Normally, this includes messages that start with the following:
+Given the user that identifies the bot in a group chat and text that was just
+sent to the chat, this detects if the message was directed at the bot. Normally,
+this includes messages that start with the following:
 
   nick: ...
   nick, ...
@@ -453,7 +455,11 @@ and infix references like this:
 
   ..., nick, ...
 
-If you want something different, you may subclass service and override this method.
+If you want something different, you may subclass service and override this
+method.
+
+Note that the text is sent as a reference and can be modified, usually to remove
+the nick from the message so the bot does not have to worry about that.
 
 =cut
 
@@ -461,10 +467,10 @@ sub is_to_me {
     my ($self, $me_user, $text) = @_;
 
     my $me_nick = $me_user->nick;
-    return scalar $text =~ s/^ $me_nick \s* [:,\-]
-                            |  , \s* $me_nick [.]? $
-                            |  , \s* $me_nick \s* , 
-                            //x;
+    return scalar($$text =~ s/^ $me_nick \s* [:,\-]
+                             |  , \s* $me_nick [.]? $
+                             |  , \s* $me_nick \s* , 
+                             //x);
 }
 
 =head2 got_group_message
@@ -507,7 +513,7 @@ sub got_group_message {
     # See if the group message is talking to us...
     my $to_identity;
     my $text    = $xmpp_message->body;
-    if ($self->is_to_me($me_user, $text)) {
+    if ($self->is_to_me($me_user, \$text)) {
         $to_identity = Bot::Backbone::Identity->new(
             username => $me_user->real_jid // $me_user->in_room_jid,
             nickname => $me_user->nick,
